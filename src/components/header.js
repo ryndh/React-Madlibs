8a90c8e654bdd4a31bf9128bd019e4f9fe6b3b1e
ryanhull <ryhu23@gmail.com>
2019-02-20 12:09:55 -0700

    header change

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = props => {
     return (
     <div className='header'> 
-        <h1>React Madlibs</h1>
+        <h1>React Mad Libs</h1>
         <h3>{props.title}</h3>
     </div>
     )
